f38fab5c1263b1bff21cc81b23fd8bc1544576c3
dmytro.korshuk <diman.korshuk@gmail.com>
2024-02-21 10:56:46 +0200

    updated headless=False and updated yml file to fix tests running remote

--- conftest.py
+++ conftest.py
@@ -17,7 +17,7 @@ PASSWORD = os.environ['PASSWORD']
 @pytest.fixture(scope="function")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
 
