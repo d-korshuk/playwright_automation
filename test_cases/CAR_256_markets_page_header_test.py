930800520dfc45790b433d00835be217a78ae02a
dmytro.korshuk <diman.korshuk@gmail.com>
2024-02-21 17:33:39 +0200

    added url verifications for car-256

--- test_cases/CAR_256_markets_page_header_test.py
+++ test_cases/CAR_256_markets_page_header_test.py
@@ -1,5 +1,6 @@
+import os
 
-#import allure
+# import allure
 import pytest
 from playwright.sync_api import expect
 
@@ -7,27 +8,26 @@ from playwright.sync_api import expect
 @pytest.mark.usefixtures("setup")
 class TestPageHeader:
 
-    #@allure.title("Check the markets page header/logo and available tabs - C35686")
+    # @allure.title("Check the markets page header/logo and available tabs - C35686")
+    @pytest.mark.test11
     def test_tab_header(self, login_to_app, markets_page, login_page):
-
         expect(markets_page.page_logo).to_be_visible()
         expect(markets_page.page_title).to_be_visible()
         expect(markets_page.overview_tab_locator).to_have_attribute("aria-selected", "true")
+        expected_url = os.environ["URL"] + "/dashboard/market-sizing/overview"
+        expect(markets_page.page).to_have_url(expected_url)
 
         markets_page.open_sales_tab()
         expect(markets_page.sales_tab_locator).to_have_attribute("aria-selected", "true")
+        expected_url = os.environ["URL"] + "/dashboard/market-sizing/sales"
+        expect(markets_page.page).to_have_url(expected_url)
 
         markets_page.open_pipeline_tab()
         expect(markets_page.pipeline_tab_locator).to_have_attribute("aria-selected", "true")
+        expected_url = os.environ["URL"] + "/dashboard/market-sizing/pipeline"
+        expect(markets_page.page).to_have_url(expected_url)
 
         markets_page.open_company_tab()
         expect(markets_page.company_tab_locator).to_have_attribute("aria-selected", "true")
-
-
-
-
-
-
-
-
-
+        expected_url = os.environ["URL"] + "/dashboard/market-sizing/company"
+        expect(markets_page.page).to_have_url(expected_url)
