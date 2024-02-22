e0d75c56a8b5a95f351fdf07a90ecbfe0582d134
dmytro.korshuk <diman.korshuk@gmail.com>
2024-02-22 15:06:03 +0200

    uncommented login_page.click_continue_wo_pass() to fix tests

--- test_cases/CAR_781_overview_tab_metrics_test.py
+++ test_cases/CAR_781_overview_tab_metrics_test.py
@@ -9,7 +9,7 @@ class TestOverviewMetrics:
     @pytest.mark.test44
     def test_overview_tab_metrics(self, login_to_app, markets_page):
         expect(markets_page.wwsales_2022_locator).to_have_text("$944m")
-        expect(markets_page.wwsales_2028_locator).to_have_text("$1,320m")
+        expect(markets_page.wwsales_2028_locator).to_have_text("$1,327m")
         expect(markets_page.wwsales_2028_diff_locator).to_have_text("+376m")
         expect(markets_page.unique_act_products_locator).to_have_text("4,612")
         expect(markets_page.active_companies_locator).to_have_text("714")
