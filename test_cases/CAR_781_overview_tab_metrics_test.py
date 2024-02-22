9a75b66cf15aec090236876a468f70238ca871c7
dmytro.korshuk <diman.korshuk@gmail.com>
2024-02-22 15:51:49 +0200

    updated metrics values to pass tests

--- test_cases/CAR_781_overview_tab_metrics_test.py
+++ test_cases/CAR_781_overview_tab_metrics_test.py
@@ -12,6 +12,6 @@ class TestOverviewMetrics:
         expect(markets_page.wwsales_2028_locator).to_have_text("$1,327m")
         expect(markets_page.wwsales_2028_diff_locator).to_have_text("+383m")
         expect(markets_page.unique_act_products_locator).to_have_text("4,632")
-        expect(markets_page.active_companies_locator).to_have_text("714")
+        expect(markets_page.active_companies_locator).to_have_text("717")
         expect(markets_page.top_company_locator).to_have_text("Pfizer")
         expect(markets_page.top_product_locator).to_have_text("Comirnaty")
