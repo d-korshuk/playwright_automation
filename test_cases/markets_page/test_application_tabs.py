917ba517f29309d8229604f4dbf42d3e44724bee
dmytro.korshuk <diman.korshuk@gmail.com>
2024-03-14 17:32:19 +0200

    added tests to check the expand/collapse functionality and switching between widget tabs

---
+++ test_cases/markets_page/test_application_tabs.py
@@ -0,0 +1,23 @@
+import pytest
+from playwright.sync_api import expect
+
+
+@pytest.mark.usefixtures("setup")
+class TestAppTabs:
+
+    def test_switch_tabs(self, login_to_app, markets_page):
+        markets_page.open_overview_tab()
+
+        markets_page.select_companies_tab()
+
+        expect(markets_page.top_by_sales_companies_tab_locator).to_have_attribute("aria-pressed", "true")
+
+        markets_page.select_technology_tab()
+        expect(markets_page.top_by_sales_technology_tab_locator).to_have_attribute("aria-pressed", "true")
+
+        markets_page.select_moa_tab()
+        expect(markets_page.top_by_sales_moa_tab_locator).to_have_attribute("aria-pressed", "true")
+
+        markets_page.select_product_tab()
+        expect(markets_page.top_by_sales_product_tab_locator).to_have_attribute("aria-pressed", "true")
+
--- test_cases/markets_page/test_widgets.py
+++ test_cases/markets_page/test_widgets.py
@@ -3,24 +3,22 @@ from playwright.sync_api import expect
 
 
 @pytest.mark.usefixtures("setup")
-class TestWidget:
+class TestWidgets:
 
-    def test_switch_tabs(self, login_to_app, markets_page):
-        markets_page.open_overview_tab()
+    def test_expand_narrow_widget(self, login_to_app, markets_page):
+        expect(markets_page.develop_status_expand_icon_locator).to_be_visible()
 
-        markets_page.select_companies_tab()
+        markets_page.develop_status_expand_icon_locator.click()
+        expect(markets_page.develop_status_collapse_icon_locator).to_be_visible()
+        expect(markets_page.develop_status_expand_icon_locator).not_to_be_visible()
 
-        expect(markets_page.top_by_sales_companies_tab_locator).to_have_attribute("aria-pressed", "true")
+        markets_page.develop_status_collapse_icon_locator.click()
+        expect(markets_page.develop_status_expand_icon_locator).to_be_visible()
 
-        markets_page.select_technology_tab()
-        expect(markets_page.top_by_sales_technology_tab_locator).to_have_attribute("aria-pressed", "true")
+    def test_switch_widget_tabs(self, login_to_app, markets_page):
+        expect(markets_page.develop_status_prod_count_tab_locator).to_have_attribute("aria-pressed", "true")
+        expect(markets_page.develop_status_prod_w_sales_locator).to_have_attribute("aria-pressed", "false")
 
-        markets_page.select_moa_tab()
-        expect(markets_page.top_by_sales_moa_tab_locator).to_have_attribute("aria-pressed", "true")
-
-        markets_page.select_product_tab()
-        expect(markets_page.top_by_sales_product_tab_locator).to_have_attribute("aria-pressed", "true")
-
-
-
-
+        markets_page.develop_status_prod_w_sales_locator.click()
+        expect(markets_page.develop_status_prod_count_tab_locator).to_have_attribute("aria-pressed", "false")
+        expect(markets_page.develop_status_prod_w_sales_locator).to_have_attribute("aria-pressed", "true")
